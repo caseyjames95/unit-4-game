066f181db5349aa40614e71d2be7dfc1cdf9a58e
caseyjames95 <caseypurves@gmail.com>
2019-09-06 21:58:41 -0700

    Javascript setup
    Setup basic javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+// Arrays for target and gem values
+const targetValues = []
+const gemValues = []
+
+// Start and select values for target and gems
+const gameSelect = function () {
+    
+}
+
+// Basic game variables
+let wins = 0
+let loss = 0
+let gemValue = []
+let green = 0
+let blue = 0
+let yellow = 0
+let res = 0
+
